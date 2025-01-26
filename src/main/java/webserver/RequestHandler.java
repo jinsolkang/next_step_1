1dac4288e022f36c8e16a3b5797c320532580795
강진솔 <jinsolkang@i-nara.co.kr>
2025-01-26 11:20:34 +0900

    task1

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -24,19 +23,45 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String header = read_header(in);
+            String filePath = "./webapp" + get_url(header);
+            File file = get_file(filePath);
+
+            response(out, file);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private String read_header(InputStream in) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        return br.lines()
+                .takeWhile(line -> !line.isEmpty())
+                .collect(Collectors.joining("\n"));
+    }
+
+    private String get_url(String header) {
+        return header.split(" ")[1];
+    }
+
+    private File get_file(String filePath) {return new File(filePath);}
+
+    private void response(OutputStream out, File file) throws IOException {
+        DataOutputStream dos = new DataOutputStream(out);
+        String content_type = "text/html;charset=utf-8";
+        byte[] body = (file.exists() && file.isFile()) ? Files.readAllBytes(file.toPath()) : "Hello World".getBytes();
+
+        log.info("Request URL: {}", file.getPath());
+        log.info("Response Status: 200 OK");
+
+        response200Header(dos, body.length, content_type);
+        responseBody(dos, body);
+    }
+
+    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) throws IOException {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + contentType + "\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -49,6 +74,7 @@ public class RequestHandler extends Thread {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
+            log.info("error body");
             log.error(e.getMessage());
         }
     }
